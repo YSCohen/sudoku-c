7c4ee6ef68d7376f4601f7f0ab53e79fbb71e923
Yehuda Cohen <78832113+YSCohen@users.noreply.github.com>
2026-09-17 18:50:38 -0400

    implement both `board_cell` fns

--- src/board.c
+++ src/board.c
@@ -46,18 +46,24 @@ void board_destroy(SudokuBoard **board_ptr) {
 
 int *board_cell(SudokuBoard *board, int row, int column) {
     /* STUDENT TODO 2: Return the mutable cell pointer for this coordinate. */
-    (void)board;
-    (void)row;
-    (void)column;
-    return NULL;
+    if (board == NULL || !board_coordinates_in_range(row, column))
+    {
+        return NULL;
+    }
+    
+    int idx = (9 * row) + column;
+    return &(board->cells[idx]);
 }
 
 const int *board_cell_const(const SudokuBoard *board, int row, int column) {
     /* STUDENT TODO 2: Return the read-only cell pointer for this coordinate. */
-    (void)board;
-    (void)row;
-    (void)column;
-    return NULL;
+    if (board == NULL || !board_coordinates_in_range(row, column))
+    {
+        return NULL;
+    }
+
+    int idx = (9 * row) + column;
+    return &(board->cells[idx]);
 }
 
 void board_clear(SudokuBoard *board) {
